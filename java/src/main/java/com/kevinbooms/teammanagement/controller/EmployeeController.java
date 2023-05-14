5128eeb045fc7f2f271245949b96a514e847476c
Kevin Booms <mrkbooms@gmail.com>
2023-05-13 21:39:51 -0400

    Setup axios and added CORS to EmployeeController

--- java/src/main/java/com/kevinbooms/teammanagement/controller/EmployeeController.java
+++ java/src/main/java/com/kevinbooms/teammanagement/controller/EmployeeController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/api/employees")
 public class EmployeeController {
