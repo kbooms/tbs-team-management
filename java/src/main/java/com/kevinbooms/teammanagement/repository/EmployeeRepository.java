5b2b8958ede6bfa7b7efd2193b770cf0513ab1d7
Kevin Booms <mrkbooms@gmail.com>
2023-05-17 12:28:58 -0400

    Added commenting for Employee Model and Repository

--- java/src/main/java/com/kevinbooms/teammanagement/repository/EmployeeRepository.java
+++ java/src/main/java/com/kevinbooms/teammanagement/repository/EmployeeRepository.java
@@ -4,5 +4,7 @@ import com.kevinbooms.teammanagement.model.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
-
+    // JpaRepository interface includes CrudRepository and PagingAndSorting Repository
+    // In this manner we have created a repository that takes Employee Objects and a Long
+    // for the Id
 }
